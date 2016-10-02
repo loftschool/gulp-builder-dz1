26873c575342684912db34602d80ed560fc13667
Maksim <mneliseev@gmail.com>
2016-10-02 23:43:34 +0500

    Изменения № 2
    Частично выпоненная вертска

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,30 @@
 (function() {
   'use strict';
 
+    var navMain = document.querySelector('.main-nav');
+    var navToggle = document.querySelector('.main-nav__toggle');
+    var mainlist = document.querySelector('.main-nav__list');
+
+    navMain.classList.remove('main-nav--nojs');
+
+    navToggle.addEventListener('click', function() {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+        mainlist.classList.remove('main-nav__list');
+        mainlist.classList.add('main-nav__list--opened');
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+        mainlist.classList.add('main-nav__list');
+        mainlist.classList.remove('main-nav__list--opened');
+      }
+    });
+
   setTimeout(function() {
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
 })();
+
+
+
