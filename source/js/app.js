1c613928d938c583ad1bbf33f8cc8ec78cd43097
Maksim <mneliseev@gmail.com>
2016-10-09 12:57:55 +0500

    Изменение 10
    Прелоадер, измения в оформление

--- source/js/app.js
+++ source/js/app.js
@@ -1,26 +1,51 @@
-(function() {
-  'use strict';
+$(function() {
 
-    var navMain = document.querySelector('.main-nav');
-    var navToggle = document.querySelector('.main-nav__toggle');
-    var mainlist = document.querySelector('.main-nav__list');
+  var imgs = [];
 
-    navMain.classList.remove('main-nav--nojs');
+  $.each($('*'), function () {
+    var
+      $this = $(this),
+      background = $this.css('background-image');
+      img = $this.is('img');
 
-    navToggle.addEventListener('click', function() {
-      if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
-        mainlist.classList.remove('main-nav__list');
-        mainlist.classList.add('main-nav__list--opened');
-      } else {
-        navMain.classList.add('main-nav--closed');
-        navMain.classList.remove('main-nav--opened');
-        mainlist.classList.add('main-nav__list');
-        mainlist.classList.remove('main-nav__list--opened');
+    if (background != 'none') {
+
+      var path = background.replace('url("', '').replace('")', '');
+      imgs.push(path);
+    }
+
+    if (img) {
+      var path = $this.attr('src');
+
+      if (path) {
+        imgs.push(path);
+      }
+    }
+
+  });
+
+  var percentsTotal = 1;
+
+  for (var i = 0; i < imgs.length; i++) {
+    var image = $('<img>', {
+      attr: {
+          src: imgs[i]
       }
     });
-})();
 
+    image.on('load', function () {
+      setPercents(imgs.length, percentsTotal);
+      percentsTotal++;
+    });
+  }
 
+  function setPercents(total, current) {
+    var percent = Math.ceil(current/ total * 100);
 
+    if(percent >= 100) {
+      $('.preloader').fadeOut();
+    }
+
+    $('.preloader__percents').text(percent + '%');
+  }
+});
