ed4dedbbac3a2995ab74e9026245c3fbedc63437
Maksim <mneliseev@gmail.com>
2016-10-09 15:27:54 +0500

    Изменения № 11
    Переделал стек техноогий и добавил уровень владения ими

--- source/js/app.js
+++ source/js/app.js
@@ -49,3 +49,28 @@ $(function() {
     $('.preloader__percents').text(percent + '%');
   }
 });
+
+(function() {
+
+'use strict';
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var mainlist = document.querySelector('.main-nav__list');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+  navMain.classList.remove('main-nav--closed');
+  navMain.classList.add('main-nav--opened');
+  mainlist.classList.remove('main-nav__list');
+  mainlist.classList.add('main-nav__list--opened');
+
+  } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      mainlist.classList.add('main-nav__list');
+      mainlist.classList.remove('main-nav__list--opened');
+  }
+});
+
+})();
