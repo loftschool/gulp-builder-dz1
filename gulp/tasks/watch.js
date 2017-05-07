a6b1ef859591aa538463a408eb8ad621cf468f1d
Anton <anton-loftschool@gunov.name>
2017-05-07 18:01:56 +0700

    added footer-copyright block

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,5 +6,10 @@ module.exports = function() {
     $.gulp.watch('./source/style/**/*.{sass,scss}', $.gulp.series('sass'));
     $.gulp.watch('./source/template/**/*.pug', $.gulp.series('pug'));
     $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy:image'));
+    // blocks
+    $.gulp.watch('./source/blocks/**/*.js', $.gulp.series('js:process'));
+    $.gulp.watch('./source/blocks/**/*.{sass,scss}', $.gulp.series('sass'));
+    $.gulp.watch('./source/blocks/**/*.pug', $.gulp.series('pug'));
+    $.gulp.watch('./source/blocks/**/*.*', $.gulp.series('copy:image'));
   });
 };
